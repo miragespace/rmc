3a93d78bea99f26996ffe97af06cdd29ce9d9bae
Rachel Chen <me@rachelchen.me>
2020-10-22 00:45:37 -0700

    include running/stopped count in Host

--- host/host.go
+++ host/host.go
@@ -5,7 +5,8 @@ import "time"
 // Host defines the physical/virtual server that will deploy Minecraft servers to Docker
 type Host struct {
 	Name          string `gorm:"primaryKey"`
-	Load          int64
+	Running       int64
+	Stopped       int64
 	Capacity      int64
 	LastHeartbeat time.Time
 }
