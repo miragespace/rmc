dbcc49c6e6c507195c1571a6785baaab113e56be
Rachel Chen <me@rachelchen.me>
2020-10-21 17:48:58 -0700

    defines the protocol buffer for instance control/provision

--- instance/instance.go
+++ instance/instance.go
@@ -5,8 +5,9 @@ type Instance struct {
 	ID             string `json:"id" gorm:"primaryKey"`          // UUID of the server instance
 	CustomerID     string `json:"customerId" gorm:"uniqueIndex"` // Corresponds to Stripe's customer ID
 	SubscriptionID string `json:"subscriptionId"`                // Corresponds to Stripe's subscription ID
+	Host           string `json:"host"`                          // Defines which host the server runs on
 	ServerAddr     string `json:"serverAddr"`                    // Minecraft server host IP
 	ServerPort     int    `json:"serverPort"`                    // Minecraft server port
-	State          string `json:"state"`                         // Starting/Stopping/Started/Stopped/Unknown
+	State          string `json:"state"`                         // Starting/Stopping/Started/Stopped/Unknown/Provisioning
 	Status         string `json:"status"`                        // Active/Terminated
 }
