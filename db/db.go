2fca99ba64eebb8befe4823a1661c7684430bdb2
Rachel Chen <me@rachelchen.me>
2020-10-21 19:22:15 -0700

    set parameters for database pool

--- db/db.go
+++ db/db.go
@@ -1,15 +1,25 @@
 package db
 
 import (
+	"time"
+
 	"github.com/pkg/errors"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+// New returns an instance for interacting with the PostgreSQL database
 func New(uri string) (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
 	if err != nil {
 		return nil, errors.Wrap(err, "Cannot connect to database")
 	}
+	pool, err := db.DB()
+	if err != nil {
+		return nil, errors.Wrap(err, "Cannot get the connection pool")
+	}
+	pool.SetMaxIdleConns(1)
+	pool.SetMaxOpenConns(20)
+	pool.SetConnMaxLifetime(time.Hour)
 	return db, nil
 }
