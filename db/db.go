88375ff2993cc55da1802e1df6e9031d65c5171f
Rachel Chen <me@rachelchen.me>
2020-10-23 18:18:47 -0700

    use zap for gorm logger

--- db/db.go
+++ db/db.go
@@ -4,13 +4,24 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
+	"moul.io/zapgorm2"
 )
 
 // New returns an instance for interacting with the PostgreSQL database
-func New(uri string) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
+func New(logger *zap.Logger, uri string) (*gorm.DB, error) {
+	gLogger := &zapgorm2.Logger{
+		ZapLogger:        logger,
+		LogLevel:         gormlogger.Warn,
+		SlowThreshold:    time.Second,
+		SkipCallerLookup: false,
+	}
+	db, err := gorm.Open(postgres.Open(uri), &gorm.Config{
+		Logger: gLogger,
+	})
 	if err != nil {
 		return nil, errors.Wrap(err, "Cannot connect to database")
 	}
