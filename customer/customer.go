9ffe87c22b183e417178aacaba07544e3e72e0fd
Rachel Chen <me@rachelchen.me>
2020-10-23 23:12:03 -0700

    fix index/nullable for models

--- customer/customer.go
+++ customer/customer.go
@@ -2,6 +2,6 @@ package customer
 
 // Customer describes a user in RMC
 type Customer struct {
-	ID    string `json:"id" gorm:"primaryKey"`     // Corresponds to Stripe's customer ID
-	Email string `json:"email" gorm:"uniqueIndex"` // User's email address
+	ID    string `json:"id" gorm:"primaryKey"`              // Corresponds to Stripe's customer ID
+	Email string `json:"email" gorm:"uniqueIndex;not null"` // User's email address
 }
--- instance/instance.go
+++ instance/instance.go
@@ -2,14 +2,14 @@ package instance
 
 // Instance describes a Minecraft server instance
 type Instance struct {
-	ID             string `json:"id" gorm:"primaryKey"`          // UUID of the server instance. This will also be the name of the Docker container
-	CustomerID     string `json:"customerId" gorm:"uniqueIndex"` // Corresponds to Stripe's customer ID
-	SubscriptionID string `json:"subscriptionId"`                // Corresponds to Stripe's subscription ID
-	HostName       string `json:"hostName"`                      // Defines which host the server runs on
-	ServerAddr     string `json:"serverAddr"`                    // Minecraft server host IP
-	ServerPort     int    `json:"serverPort"`                    // Minecraft server port
-	ServerVersion  string `json:"serverVersion"`                 // Minecraft server version
-	IsJavaEdition  bool   `json:"isJavaEdition"`                 // Minecraft server edition (Java/Bedrock)
-	State          string `json:"state"`                         // Starting/Stopping/Started/Stopped/Unknown/Provisioning
-	Status         string `json:"status"`                        // Active/Terminated
+	ID             string `json:"id" gorm:"primaryKey"`                       // UUID of the server instance. This will also be the name of the Docker container
+	CustomerID     string `json:"customerId" gorm:"index;not null"`           // Corresponds to Stripe's customer ID
+	SubscriptionID string `json:"subscriptionId" gorm:"uniqueIndex;not null"` // Corresponds to Stripe's subscription ID
+	HostName       string `json:"hostName"`                                   // Defines which host the server runs on
+	ServerAddr     string `json:"serverAddr"`                                 // Minecraft server host IP, usually the same as the host's ip
+	ServerPort     uint32 `json:"serverPort"`                                 // Minecraft server port
+	ServerVersion  string `json:"serverVersion"`                              // Minecraft server version
+	IsJavaEdition  bool   `json:"isJavaEdition"`                              // Minecraft server edition (Java/Bedrock)
+	State          string `json:"state"`                                      // Starting/Stopping/Started/Stopped/Unknown/Provisioning
+	Status         string `json:"status"`                                     // Active/Terminated
 }
