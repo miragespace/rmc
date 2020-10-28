535eb8a3f4e08a1dac7c8605d46fc94c38408715
Rachel Chen <me@rachelchen.me>
2020-10-27 23:57:10 -0700

    added comments for spec/parameters.go and make a copy of the map when convert from/to protobuf

--- spec/parameters.go
+++ spec/parameters.go
@@ -11,8 +11,10 @@ import (
 	"gorm.io/gorm/schema"
 )
 
+// Parameters defines a key-value type for used in models and protobuf
 type Parameters map[string]string
 
+// Scan is used for the sql driver to load from JSON blob into Golang's map data structure
 func (p *Parameters) Scan(value interface{}) error {
 	bytes, ok := value.([]byte)
 	if !ok {
@@ -25,10 +27,12 @@ func (p *Parameters) Scan(value interface{}) error {
 	return json.Unmarshal(bytes, &p)
 }
 
+// Value is used for the sql driver to serialize Parameters into JSON blob to be stored
 func (p *Parameters) Value() (driver.Value, error) {
 	return json.Marshal(p)
 }
 
+// GormDBDataType is gorm package specific, and returning the corresponding column data type depending on the database
 func (*Parameters) GormDBDataType(db *gorm.DB, field *schema.Field) string {
 	switch db.Dialector.Name() {
 	case "mysql", "sqlite":
@@ -39,6 +43,7 @@ func (*Parameters) GormDBDataType(db *gorm.DB, field *schema.Field) string {
 	return ""
 }
 
+// Clone will return a copy of itself
 func (p *Parameters) Clone() Parameters {
 	clone := make(Parameters)
 	for k, v := range *p {
@@ -47,12 +52,18 @@ func (p *Parameters) Clone() Parameters {
 	return clone
 }
 
+// ToProto will convert Parameters into protobuf data type
 func (p *Parameters) ToProto() *protocol.Parameters {
 	return &protocol.Parameters{
-		Data: *p,
+		Data: p.Clone(),
 	}
 }
 
+// FromProto will convert protobuf data into Parameters
 func (p *Parameters) FromProto(pb *protocol.Parameters) {
-	*p = pb.Data
+	clone := make(map[string]string)
+	for k, v := range pb.Data {
+		clone[k] = v
+	}
+	*p = clone
 }
