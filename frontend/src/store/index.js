ac99d2f1a48e10ba4ac79664176b42bec06552e3
Rachel Chen <rachel@chens.email>
2020-11-27 01:02:46 -0800

    frontend: support for jwt refresh token

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Router from '../router'
+import { getHeader } from './helper'
 
 Vue.use(Vuex)
 
@@ -9,42 +10,109 @@ const BASE_URL = process.env.VUE_APP_API_ENDPOINT;
 export default new Vuex.Store({
     state: {
         brandName: process.env.VUE_APP_BRAND_NAME || 'RMC',
-        bearerToken: localStorage.getItem("token") || ''
+        accessToken: localStorage.getItem("accessToken") || '',
+        refreshToken: localStorage.getItem("refreshToken") || ''
     },
     getters: {
         isLogin(state) {
-            return state.bearerToken !== ''
+            return state.accessToken !== ''
         }
     },
     mutations: {
-        setBearerToken(state, payload) {
-            state.bearerToken = payload.token
-            localStorage.setItem('token', payload.token)
+        setTokens(state, payload) {
+            state.accessToken = payload.accessToken
+            state.refreshToken = payload.refreshToken
+            localStorage.setItem('accessToken', payload.accessToken)
+            localStorage.setItem('refreshToken', payload.refreshToken)
+        },
+        updateAccessToken(state, payload) {
+            state.accessToken = payload.accessToken
+            localStorage.setItem('accessToken', payload.accessToken)
         }
     },
     actions: {
         logout(context) {
-            context.state.bearerToken = ''
-            localStorage.removeItem('token')
+            context.state.accessToken = ''
+            context.state.refreshToken = ''
+            localStorage.removeItem('accessToken')
+            localStorage.removeItem('refreshToken')
             Router.push({
                 name: 'Home'
             })
         },
-        async makeAuthenticatedRequest(context, payload) {
-            let method = payload.method || "GET"
+        async requestTokens(context, payload) {
             let req = {
-                method: method,
+                method: 'POST',
+                mode: 'cors',
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    uid: payload.uid,
+                    token: payload.token
+                })
+            }
+            let resp = await fetch(BASE_URL + '/auth/requestTokens', req)
+            let json = await resp.json()
+            if (resp.status === 200) {
+                context.commit({
+                    type: "setTokens",
+                    accessToken: json.result.accessToken,
+                    refreshToken: json.result.refreshToken,
+                })
+            } else {
+                throw {
+                    apiError: true,
+                    err: json.error,
+                    message: json.messages.join(' - ')
+                }
+            }
+        },
+        async refreshSession(context) {
+            let req = {
+                method: 'POST',
                 mode: "cors",
                 credentials: "include",
                 headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + context.state.bearerToken
+                    "Content-Type": "application/json"
                 },
-            };
-            if (method != 'GET') {
-                req.body = JSON.stringify(payload.body)
+                body: JSON.stringify({
+                    refreshToken: context.state.refreshToken
+                })
             }
-            return fetch(BASE_URL + payload.endpoint, req);
+            let resp = await fetch(BASE_URL + '/auth/refresh', req)
+            let json = await resp.json()
+            if (resp.status === 200) {
+                context.commit({
+                    type: "updateAccessToken",
+                    accessToken: json.result.accessToken
+                })
+            } else {
+                throw {
+                    apiError: true,
+                    err: json.error,
+                    message: json.messages.join(' - ')
+                }
+            }
+        },
+        async makeAuthenticatedRequest(context, payload) {
+            let resp = await fetch(BASE_URL + payload.endpoint, getHeader(context, payload));
+            if (resp.status === 401) {
+                console.log("expired access token")
+                try {
+                    await context.dispatch({
+                        type: 'refreshSession'
+                    })
+                    console.log("access token refreshed")
+                    return fetch(BASE_URL + payload.endpoint, getHeader(context, payload))
+                } catch (err) {
+                    console.log("access token refresh failed")
+                    return resp
+                }
+            } else {
+                return resp
+            }
         }
     },
     modules: {
--- frontend/src/views/VerifyToken.vue
+++ frontend/src/views/VerifyToken.vue
@@ -9,8 +9,6 @@
 import * as Sentry from "@sentry/browser";
 import Alert from "../components/Alert.vue";
 
-const BASE_URL = process.env.VUE_APP_API_ENDPOINT;
-
 export default {
   name: "VerifyToken",
   components: {
@@ -30,36 +28,35 @@ export default {
     let uid = this.$route.params.uid;
     let token = this.$route.params.token;
 
-    let data = BASE_URL + "/customers/" + uid + "/" + token;
     try {
-      let resp = await fetch(data);
-      let json = await resp.json();
-
-      if (resp.status == 200) {
-        this.$refs.alert.showAlert(
-          "success",
-          "Login successful! Redirecting you to instances page..."
-        );
-
-        await this.delay(2000);
-
-        await this.$store.commit({
-          type: "setBearerToken",
-          token: json.result.token,
-        });
-
-        this.$router.push({
-          name: "Instances",
-        });
-      } else {
-        this.$refs.alert.showDismissable("danger", json.error);
-      }
-    } catch (err) {
-      Sentry.captureException(err);
-      this.$refs.alert.showDismissable(
-        "danger",
-        "An unexpected error has occured: " + err.message
+      await this.$store.dispatch({
+        type: "requestTokens",
+        uid: uid,
+        token: token,
+      });
+      this.$refs.alert.showAlert(
+        "success",
+        "Login successful! Redirecting you to instances page..."
       );
+
+      await this.delay(2000);
+
+      this.$router.push({
+        name: "Instances",
+      });
+    } catch (err) {
+      if (err.apiError === true) {
+        this.$refs.alert.showDismissable(
+          "danger",
+          err.error + ": " + err.message
+        );
+      } else {
+        Sentry.captureException(err);
+        this.$refs.alert.showDismissable(
+          "danger",
+          "An unexpected error has occured: " + err.message
+        );
+      }
     }
   },
 };
